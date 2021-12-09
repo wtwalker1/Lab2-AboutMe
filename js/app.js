ae8d97098427957d50f45450ff2aba4074086d60
wtwalker1 <wtwalker1@gmail.com>
2021-12-09 16:55:04 -0600

    add prompts

--- js/app.js
+++ js/app.js
@@ -13,4 +13,16 @@ alert('Hello new friend ' + userName);
     let lowerCaseName = userName.toLowerCase();
     console.log(lowerCaseName);
 
-prompt('')
+prompt('I, Tyler, the website owner, live in Des Moines. Yes or No?')
+    let userAnswer;{
+    console.log('this is the user answer', userAnswer)
+    }
+
+    if(userAnswer = 'Yes') {
+        alert('Correct! Tyler lives in the capitol of Iowa')
+    } else {
+        alert('Actually Tyler does live in Des Moines.');
+    }
+
+prompt('Was my first job at Sonic as a cook?')
+    
