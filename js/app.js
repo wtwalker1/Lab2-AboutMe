0a8ac8983ca7e2d96eede0bde2c4b13728de92de
wtwalker1 <wtwalker1@gmail.com>
2021-12-09 17:59:58 -0600

    Correct js prompts

--- js/app.js
+++ js/app.js
@@ -1,28 +1,54 @@
 'use strict';
 console.log('app.js is connected.');
 
-let userName;
+let userName = prompt('Hello! What is your name?');
 console.log('this is the user name', userName);
 
-while(!userName){
-    userName = prompt('What is your name?');
-}
-console.log('after while loop', userName);
-
 alert('Hello new friend ' + userName);
-    let lowerCaseName = userName.toLowerCase();
-    console.log(lowerCaseName);
 
-prompt('I, Tyler, the website owner, live in Des Moines. Yes or No?')
-    let userAnswer;{
-    console.log('this is the user answer', userAnswer)
-    }
 
-    if(userAnswer = 'Yes') {
-        alert('Correct! Tyler lives in the capitol of Iowa')
-    } else {
-        alert('Actually Tyler does live in Des Moines.');
-    }
-
-prompt('Was my first job at Sonic as a cook?')
+let userAnswer = prompt("Do I live in Des Moines, IA?");
+    console.log('The user said', userAnswer);
     
+        if (userAnswer == 'Yes') {
+            alert('Correct, I live in the capitol of the Heartland');
+        } else {
+            alert('Actually I do live in Des Moines');
+        }
+
+
+let userAnswer2 = prompt('Was my first job at Sonic as a cook?');
+    console.log('The user said', userAnswer2);
+
+        if (userAnswer2 == 'Yes') {
+            alert('Correct. I worked there for two months and then literally forgot I had a job once sports started again');
+        } else {
+            alert('Sorry it was. I always left smelling like onions and did not enjoy it very much');
+        }
+    
+let userAnswer3 = prompt('Did I just move to Des Moines from Boulder, CO?');
+        console.log('The user said', userAnswer3);
+
+        if (userAnswer3 == 'Yes') {
+            alert('Correct. I was there for 3 years before moving back to Iowa 6 months ago');
+        } else {
+            alert('Actually I did. I would not lie about such a matter');
+        }
+
+let userAnswer4 = prompt('Can I juggle?');
+        console.log('The user said', userAnswer4);
+
+        if (userAnswer4 == 'Yes') {
+            alert('Correct. It is my most valuable skill');
+        } else {
+            alert('Actually I can. Hopefully I can juggle 4 balls soon');
+        }
+
+let userAnswer5 = prompt('Am I Canadian?');
+        console.log('The user said', userAnswer5);
+
+        if (userAnswer5 == 'Yes') {
+            alert('Correct. My dad is full Canadian, making me half');
+        } else {
+            alert('I am Canadian. I have a maple leaf tattooed on me to prove it');
+        }
