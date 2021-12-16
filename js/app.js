d110ca6dc2c186f8b85f54c55aaa66ca3e220ef2
wtwalker1 <wtwalker1@gmail.com>
2021-12-15 18:59:30 -0600

    add point system

--- js/app.js
+++ js/app.js
@@ -1,6 +1,8 @@
 'use strict';
 console.log('app.js is connected.');
 
+let points = 0;
+
 let userName = prompt('Hello! What is your name?');
 console.log('this is the user name', userName);
 
@@ -11,6 +13,7 @@ let userAnswer = prompt("Do I live in Des Moines, IA?");
     console.log('The user said', userAnswer);
     
         if (userAnswer == 'Yes') {
+            points +=1;
             alert('Correct, I live in the capitol of the Heartland');
         } else {
             alert('Actually I do live in Des Moines');
@@ -21,6 +24,7 @@ let userAnswer2 = prompt('Was my first job at Sonic as a cook?');
     console.log('The user said', userAnswer2);
 
         if (userAnswer2 == 'Yes') {
+            points +=1;
             alert('Correct. I worked there for two months and then literally forgot I had a job once sports started again');
         } else {
             alert('Sorry it was. I always left smelling like onions and did not enjoy it very much');
@@ -30,6 +34,7 @@ let userAnswer3 = prompt('Did I just move to Des Moines from Boulder, CO?');
         console.log('The user said', userAnswer3);
 
         if (userAnswer3 == 'Yes') {
+            points +=1;
             alert('Correct. I was there for 3 years before moving back to Iowa 6 months ago');
         } else {
             alert('Actually I did. I would not lie about such a matter');
@@ -39,6 +44,7 @@ let userAnswer4 = prompt('Can I juggle?');
         console.log('The user said', userAnswer4);
 
         if (userAnswer4 == 'Yes') {
+            points +=1;
             alert('Correct. It is my most valuable skill');
         } else {
             alert('Actually I can. Hopefully I can juggle 4 balls soon');
@@ -48,6 +54,7 @@ let userAnswer5 = prompt('Am I Canadian?');
         console.log('The user said', userAnswer5);
 
         if (userAnswer5 == 'Yes') {
+            points +=1;
             alert('Correct. My dad is full Canadian, making me half');
         } else {
             alert('I am Canadian. I have a maple leaf tattooed on me to prove it');
@@ -61,7 +68,7 @@ for (let i = 0; i < 4; i++){
     if (ageAnswer == age) {
         alert('That is correct, I am ' + age + ' years young hehe');
         console.log('User answered ' + ageAnswer);
-        correctAnswers++;
+        points +=1;
         break;
     }
     else if (ageAnswer > age && (i !== 3)) {
@@ -82,27 +89,28 @@ for (let i = 0; i < 4; i++){
     }
 }
 
-let pizzaChoices = ['Pepporoni', 'Sausage', 'Canadian Bacon', 'Cheese'];
-let pizza = 'Pepporoni';
+let pizzaChoices = ['pepperoni', 'sausage', 'canadian bacon', 'cheese'];
+let tries = 0;
 
-while (tries <=5) {
-    let answer7 = prompt('Can you guess my favorite type of pizza?').toLowerCase();
+while (tries < 5) {
+    let answer7 = prompt('Can you guess my favorite type of pizza?').toLowerCase().trim();
     for(let i = 0; i < pizzaChoices.length; i++){
-        console.log(answer7);
+        console.log('userInput',answer7);
         console.log(pizzaChoices[i]);
-        if(answer7 === answers[i]) {
+        if(answer7 === 'pepperoni') {
             alert('That is correct');
             points +=1;
-            alert('My favorite pizza is pepperoni');
             tries = 5;
             break;
         }
         if(tries !==5){
             alert('Try again');
-        }
-        tries = tries + 1;
-        if(tries === 5){
-            alert('My favorite pizza is pepperoni');
+            break;
         }
     }
+    tries = tries + 1;
 }
+if(tries === 5){
+    alert('My favorite pizza is pepperoni');
+}
+console.log(points);
